75185066b14b074cc69933dea3a16ef21474d306
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2015-01-03 02:15:56 +0100

    Improved iterate with index

--- ch10.exercises.hs
+++ ch10.exercises.hs
@@ -1,16 +1,21 @@
+-- 10.18
+
+-- filterFirst p xs = zipWith step xs $ map p xs
+
 -- 10.20
 -- switchMap addOne addTen [1,2,3,4] # => [2,12,4,14]
+
 addOne x = x + 1
 addTen x = x + 10
 
-switchMap f g xs = zipWith step xs (take (length xs) [1..])
+switchMap f g xs = zipWith step xs [1..]
     where step x index = if odd index then f x else g x
 
 -- 10.21
 -- split [1,2,3,4,5]      # => ([1,3,5], [2,4])
 -- merge ([1,3,5], [2,4]) # => [1,2,3,4,5]
 
-split xs = foldr step ([], []) $ zip xs (take (length xs) [1..])
+split xs = foldr step ([], []) $ zip xs [1..]
     where step (x, index) (oddList, evenList) = if odd index
                                                 then (x:oddList, evenList)
                                                 else (oddList, x:evenList)
